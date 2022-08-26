2cf02ac69df47327c896634766ce694d05774d3c
Mike P. Sinn <m@quantimodo.com>
2022-08-26 15:42:12 -0500

    loadEnvFromDopplerOrDotEnv
    Took 8 minutes

--- ts/qm.builder.ts
+++ ts/qm.builder.ts
@@ -2,6 +2,8 @@
 // npm install typescript ts-node
 // npx ts-node ts/gi-run.ts
 // process.env.RELEASE_STAGE = "ionic"
+import {loadEnvFromDopplerOrDotEnv} from "./env-helper"
+loadEnvFromDopplerOrDotEnv(".env")
 import {saveAppSettings} from "./qm.app-settings"
 import {writeBuildInfoFile} from "./qm.build-info-helper"
 writeBuildInfoFile().then(() => {
