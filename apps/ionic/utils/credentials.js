ace1e33e88076d25ed79ab49f2b53ece4c2439ef
Mike P. Sinn <m@quantimodo.com>
2023-02-13 01:27:02 -0600

    Added utils

---
+++ apps/ionic/utils/credentials.js
@@ -0,0 +1,33 @@
+const urlHelper = require("./urlHelper");
+const qm = require("../public/js/qmHelpers");
+const dataSources = require("../data/data-sources");
+const envHelper = require("../ts/env-helper");
+let credentials = {
+  getScopes(connectorName){
+    const dataSource = dataSources[connectorName] || null;
+    if(dataSource){
+      return dataSource.scopes;
+    } else {
+      throw Error("data/data-sources does not contain connector called: " + connectorName);
+    }
+  }
+};
+credentials.find = function(strategyName, connectorName){
+  let toUpperCase = strategyName.toUpperCase();
+  let clientID = envHelper.getRequiredEnv("CONNECTOR_" + toUpperCase + "_CLIENT_ID");
+  let clientSecret = envHelper.getRequiredEnv("CONNECTOR_" + toUpperCase + "_CLIENT_SECRET");
+  let res = {
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: `${urlHelper.serverOrigin}/auth/${strategyName}/callback`,
+    passReqToCallback: true
+  };
+  if(connectorName === "twitter" || connectorName === "withings"){
+    res.consumerKey = clientID;
+    res.consumerSecret = clientSecret;
+    delete res.clientID;
+    delete res.clientSecret;
+  }
+  return res;
+};
+module.exports = credentials;
