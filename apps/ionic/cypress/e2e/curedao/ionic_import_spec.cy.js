e79f9fad304004f097fdc60d98279488d743c8ef
Mike P. Sinn <m@quantimodo.com>
2023-02-14 09:12:43 -0600

    Cypress tests

--- apps/ionic/cypress/e2e/curedao/ionic_import_spec.cy.js
+++ apps/ionic/cypress/e2e/curedao/ionic_import_spec.cy.js
@@ -3,27 +3,34 @@
 describe('Import Data', function () {
   function goToImportPageFromInbox () {
     cy.loginWithAccessTokenIfNecessary('/#/app/reminders-inbox')
-    cy.get('a[href="#/app/import"] > i.ion-ios-cloud-download-outline').click({ force: true })
+    cy.get('#menu-item-import > a').click({ force: true })
   }
-  it.skip('Check that we can go to demo import page without logging in', function () {
+  it('Check that we can go to demo import page without logging in', function () {
     cy.visit('https://demo.quantimo.do/#/app/import')
     cy.get('#navBarAvatar > img', { timeout: 40000 })
-    cy.get('#disconnect-worldweatheronline-button', { timeout: 30000 })
-            .scrollIntoView()
-            .click()
+    // cy.get('#disconnect-worldweatheronline-button', { timeout: 30000 })
+    //         .scrollIntoView()
+    //         .click()
   })
-  it.skip('Connect Canada Weather', function () {
+  it('Connect Canada Weather', function () {
     cy.loginWithAccessTokenIfNecessary('/#/app/import')
-    cy.get('#connect-worldweatheronline-button').click()
+    cy.get('#import-data-worldweatheronline-button').click()
   })
-  it.skip('Connects Tigerview', function () {
+  it('Connects Tigerview', function () {
     goToImportPageFromInbox()
-    cy.get('#connect-tigerview-button').click()
+    cy.get('#import-data-tigerview-button').click()
   })
-  it.skip('Connects Withings', function () {
+  it('Connects Withings', function () {
     goToImportPageFromInbox()
-    cy.get('#connect-withings-button', { timeout: 30000 }).click({ force: true })
-    cy.url().should('contain', 'upgrade')
+    cy.get("#withings-item", { timeout: 30000 })
+    if (Cypress.$('#disconnect-withings-button').length > 0) {
+      cy.get('#disconnect-withings-button', { timeout: 30000 }).click({ force: true })
+      //cy.url().should('contain', 'upgrade')
+    } else {
+      cy.log("#disconnect-withings-button does not exist")
+    }
+    cy.get('#import-data-withings-button', { timeout: 30000 }).click({ force: true })
+    //cy.url().should('contain', 'upgrade')
     // TODO: Implement check for upgrade stuff
     // cy.get('input[name="email"]').click({ force: true });
     // cy.get('input[name="password"]').click({ force: true });
