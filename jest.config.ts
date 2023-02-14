e5cca3a371e0395b21e2c5349e0a8602b23b864b
Mike P. Sinn <m@quantimodo.com>
2023-02-14 09:12:17 -0600

    Fitbit login

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,5 @@ import { getJestProjects } from '@nrwl/jest';
 
 export default {
   projects: getJestProjects(),
+  // testTimeout: 20000, Doesn't work here or in tests with jest.setTimeout(30000). Have to set in jest.preset.
 };
--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = { ...nxPreset, testTimeout: 10000 };
