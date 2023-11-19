c048c65bc6103601db42a0af45e1586e946201ea
Mike P. Sinn <m@quantimodo.com>
2023-11-19 13:19:10 -0600

    dFDA model Schema

--- libs/db-schema/v1-alpha/mysql/035-property_tags.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists property_tags
-(
-    id          int auto_increment
-        primary key,
-    name        varchar(100) not null,
-    description varchar(100) not null
-);
-
