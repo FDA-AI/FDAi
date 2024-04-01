68bd67b3fee9ec6f53fb42a55fdddf071e488ca0
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-01 10:15:44 -0500

    Refactor createFdaiUser function
    The parameter name in createFdaiUser function was changed from 'clientUserId' to 'yourSystemUserId'. Additionally, the way how client ID and client secret are passed to the API was modified. They are now passed as headers rather than as part of the request body. This change enhances security by preventing sensitive data from being explicitly included in the request body.

--- libs/fdai-auth/src/lib/createFdaiUser.ts
+++ libs/fdai-auth/src/lib/createFdaiUser.ts
@@ -7,24 +7,21 @@ const FDAI_CLIENT_SECRET = process.env['FDAI_CLIENT_SECRET'] || 'oauth_test_secr
 
 /**
  * Creates an FDAi user and returns the user ID.
- * @param {string} clientUserId - The unique identifier for the user in the client's system.
+ * @param {string} yourSystemUserId - The unique identifier for the user in the client's system.
  * @returns {Promise<string>} The FDAi user ID.
  */
-export async function createFdaiUser(clientUserId: string): Promise<string> {
-    const url = `${FDAI_API_ORIGIN}/api/v1/user`;
-    const data = {
-        clientUserId,
-        clientId: FDAI_CLIENT_ID,
-        clientSecret: FDAI_CLIENT_SECRET,
-    };
-
+export async function createFdaiUser(yourSystemUserId: string): Promise<string> {
     try {
-        const response = await fetch(url, {
+        const response = await fetch(`${FDAI_API_ORIGIN}/api/v1/user`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'X-Client-Id': process.env['FDAI_CLIENT_ID'] || 'oauth_test_client',
+                'X-Client-Secret': process.env['FDAI_CLIENT_SECRET'] || 'oauth_test_secret',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify({
+              clientUserId: yourSystemUserId
+            }),
         });
 
         if (!response.ok) {
