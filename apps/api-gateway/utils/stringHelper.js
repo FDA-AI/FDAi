404058957db7e831757b6449f1e60db7d3863e7c
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-04 15:16:37 -0500

    Refactoring associated with PostgreSQL support, Jest testing, and Docker/env configurations
    Updated several .js modules including urlHelper.js, credentials.js, and qmHelper.js for improved code clarity and functionality. These changes are associated with the introduction of PostgreSQL support and Jest testing, as well as the implementation of revised Docker and environment configurations.

--- apps/api-gateway/utils/stringHelper.js
+++ apps/api-gateway/utils/stringHelper.js
@@ -15,7 +15,23 @@ function camelCaseKeys (object) {
       return carry
     }, {})
 }
+/**
+ * Replace all occurrences of `find` in `str` with `replace`.
+ *
+ * @param {String} str
+ * @param {String} find
+ * @param {String} replace
+ *
+ * @returns {String}
+ */
+function replaceAll(str, find, replace){
+  function escapeRegExp(str){
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
 
 module.exports = {
-  camelCaseKeys
+  camelCaseKeys,
+  replaceAll
 }
--- apps/api-gateway/utils/urlHelper.js
+++ apps/api-gateway/utils/urlHelper.js
@@ -1,19 +1,17 @@
 const {numberFormat} = require("underscore.string");
-const envHelper = require("../../ionic/ts/env-helper");
 //envHelper.loadEnvFromDopplerOrDotEnv(null);
-var port = envHelper.getEnvOrException("EXPRESS_PORT");
+var port = process.env.EXPRESS_PORT;
 let fallbackServerUrl = `http://localhost:${port}`;
 let envs = process.env
-let envNames = envHelper.envNames
-let apiOrigin = process.env[envNames.QM_API_ORIGIN]
+let apiOrigin = process.env.QM_API_ORIGIN
 if(!apiOrigin){
   apiOrigin = 'https://app.quantimo.do';
 }
 module.exports = {
   serverPort: port,
-  serverOrigin: process.env[envHelper.envNames.EXPRESS_ORIGIN] || fallbackServerUrl,
+  serverOrigin: process.env[process.env.EXPRESS_ORIGIN] || fallbackServerUrl,
   QM_API_ORIGIN: apiOrigin,
-  loginSuccessRedirect: process.env[envHelper.envNames.LOGIN_SUCCESS_REDIRECT] || "/#/app/onboarding",
+  loginSuccessRedirect: process.env[process.env.LOGIN_SUCCESS_REDIRECT] || "/#/app/onboarding",
   loginFailureRedirect: "/#/app/login",
   loginPath: "/#/app/login"
 }
