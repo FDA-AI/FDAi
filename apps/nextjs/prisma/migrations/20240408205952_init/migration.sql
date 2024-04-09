d185b9d4f2182d932f87b29292263aa669182320
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-09 15:10:44 -0500

    Create new database tables and update UserVariable model
    This commit introduces new tables in the database, including 'accounts', 'sessions', 'users', 'verification_tokens', 'activities', and 'activity_log'. It also involves creating several indices and adding foreign keys in 'accounts', 'sessions', 'activities', and 'activity_log'. Moreover, the UserVariable model is updated to ensure better type checking.

---
+++ apps/nextjs/prisma/migrations/20240408205952_init/migration.sql
@@ -0,0 +1,132 @@
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "fdai_user_id" INTEGER,
+    "fdai_access_token" TEXT,
+    "fdai_refresh_token" TEXT,
+    "fdai_expires_in" INTEGER,
+    "fdai_scope" TEXT,
+    "fdai_access_token_expires_at" TIMESTAMP(3),
+    "address" TEXT,
+    "banned" BOOLEAN,
+    "birthday" TIMESTAMP(3),
+    "city" TEXT,
+    "country_code" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "email_validated" BOOLEAN NOT NULL DEFAULT false,
+    "first_name" TEXT,
+    "gdpr_consent" BOOLEAN NOT NULL DEFAULT false,
+    "gender" TEXT,
+    "image" TEXT,
+    "ip_address" VARCHAR(45),
+    "language" TEXT,
+    "last_name" TEXT,
+    "last_sign_in_at" BIGINT,
+    "name" TEXT,
+    "newsletter_subscribed" BOOLEAN NOT NULL DEFAULT false,
+    "phone_number" TEXT,
+    "postal_code" TEXT,
+    "referrer_user_id" TEXT,
+    "state_province" TEXT,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "username" TEXT NOT NULL,
+    "web3_wallet" TEXT,
+    "unsafe_metadata" JSONB,
+    "public_metadata" JSONB,
+    "private_metadata" JSONB,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verification_tokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "activities" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "color_code" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "activities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "activity_log" (
+    "id" TEXT NOT NULL,
+    "activity_id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "count" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "activity_log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_tokens_token_key" ON "verification_tokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_tokens_identifier_token_key" ON "verification_tokens"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activities" ADD CONSTRAINT "activities_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activity_log" ADD CONSTRAINT "activity_log_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
