e435721122608a1f5953b12a4952ce9c7cbd9caf
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-16 11:30:01 -0500

    code cleanup is done by removing debugging outputs, adding necessary information, and minor text updates for better user experience.

--- apps/nextjs/components/loader.tsx
+++ apps/nextjs/components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
         />
       </div>
       <p className="text-sm text-muted-foreground">
-        Wishonia is thinking...
+        FDAi is thinking...
       </p>
     </div>
   );
--- apps/nextjs/config/site.ts
+++ apps/nextjs/config/site.ts
@@ -3,13 +3,15 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "FDAi",
+  name: "The Decentralized FDA",
+  author: "The Decentralized FDA",
   description:
     "Using a to determine the effects of every food and drug in the world!",
   keywords: [
   ],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
+    author: "The Decentralized FDA",
   },
   links: {
     github: "https://github.com/FDA-AI/FDAi",
