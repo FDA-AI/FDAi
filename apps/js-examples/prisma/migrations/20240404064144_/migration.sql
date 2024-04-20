eb72806bb048d4ab7936ca7e54bfe516078a9a6f
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-20 16:01:17 -0500

    JS examples

--- apps/js-examples/prisma/migrations/20240404064144_/migration.sql
+++ apps/js-examples/prisma/migrations/20240404064144_/migration.sql
@@ -2,14 +2,14 @@
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "email" TEXT,
-    "fdai_user_id" INTEGER,
+    "dfda_user_id" INTEGER,
     "first_name" TEXT,
     "username" TEXT,
-    "fdai_access_token" TEXT,
-    "fdai_refresh_token" TEXT,
-    "fdai_expires_in" INTEGER,
-    "fdai_scope" TEXT,
-    "fdai_access_token_expires_at" TIMESTAMP(3),
+    "dfda_access_token" TEXT,
+    "dfda_refresh_token" TEXT,
+    "dfda_expires_in" INTEGER,
+    "dfda_scope" TEXT,
+    "dfda_access_token_expires_at" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
