f14a7a7d802ad59294d4f03be3bb5eced6787953
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-29 20:07:14 -0600

    Add global variable validation and update user ID retrieval
    Added a new validation for global variables and improved the way user IDs are retrieved by creating a new dedicated utility function. The updated function, getUserId, replaces the prior use of getServerSession across multiple files. The new approach is expected to streamline the user authentication process and reduce code redundancy.

--- apps/nextjs/app/api/chat-with-vision/route.ts
+++ apps/nextjs/app/api/chat-with-vision/route.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-// Create an OpenAI API client (that's edge friendly!)
+// Create an OpenAI API client (that's edge-friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
 });
--- apps/nextjs/app/api/conversation2measurements/route.ts
+++ apps/nextjs/app/api/conversation2measurements/route.ts
@@ -1,35 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { text2measurements } from "@/lib/text2measurements";
-import { postMeasurements} from "@/lib/dfda";
+import { conversation2measurements } from "@/lib/conversation2measurements";
+import {postMeasurements} from "@/lib/dfda";
 import {getUserId} from "@/lib/getUserId";
 
 export async function POST(request: NextRequest) {
-  let { statement, localDateTime, text } = await request.json();
-  statement = statement || text;
+  let { statement, localDateTime, previousStatements } = await request.json();
 
 try {
-  const measurements = await text2measurements(statement, localDateTime);
+  const measurements = await conversation2measurements(statement, localDateTime, previousStatements);
   const userId = await getUserId();
   if(userId){
     await postMeasurements(measurements, userId)
   }
     return NextResponse.json({ success: true, measurements: measurements });
   } catch (error) {
-    // Log and handle any errors encountered during the request to OpenAI
-    console.error('Error sending request to OpenAI:', error);
-    return NextResponse.json({ success: false, message: 'Error sending request to OpenAI' });
+    console.error('Error in conversation2measurements:', error);
+    return NextResponse.json({ success: false, message: 'Error in conversation2measurements' });
   }
 }
 
 export async function GET(req: NextRequest) {
   const urlParams = Object.fromEntries(new URL(req.url).searchParams);
   const statement = urlParams.statement as string;
+  const previousStatements = urlParams.previousStatements as string | null | undefined;
   const localDateTime = urlParams.localDateTime as string | null | undefined;
 
   try {
-    const measurements = await text2measurements(statement, localDateTime);
+    const measurements = await conversation2measurements(statement, localDateTime, previousStatements);
     const userId = await getUserId();
-    await postMeasurements(measurements, userId);
+    if(userId){await postMeasurements(measurements, userId)}
     return NextResponse.json({ success: true, measurements: measurements });
   } catch (error) {
     console.error('Error sending request to OpenAI:', error);
